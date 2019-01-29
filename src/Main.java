2344f9716036026baee96ecee94441d462320f73
Myroslava <myroslavs.krugla@gmail.com>
2019-01-29 18:32:20 +0300

    added constructor

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main
     {
         public static void main (String[] args)
         {
-            Person Krasisik = new Person();
+            Person Krasisik = new Person(180);
             System.out.println(Krasisik.height);
+            Person Krasisia = new Person();
+            System.out.println(Krasisia.height);
+
         }
     }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,16 @@
 public class Person
     {
     public int height = 180;
-    }
+    public void say (String name)
+        {
+            System.out.println("Hello, " + name);
+        }
+        public Person ()
+        {
+        }
+    public Person (int h)
+        {
+            int height = h;
+        }
+
+}
