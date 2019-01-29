40e5f3f518c8d7b75367b9eef71ec16f1fcbb8c8
Myroslava <myroslavs.krugla@gmail.com>
2019-01-29 18:39:11 +0300

    added constructor, corrected syntax mistake

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main
     {
         public static void main (String[] args)
         {
+            Person Krasisik = new Person();
             System.out.println(Krasisik.height);
             Person Krasisia = new Person();
             System.out.println(Krasisia.height);
